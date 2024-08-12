2508f6eb804af3ab5528381ec201731bcc905370
Ramkalame <ramkalame14082001@gmail.com>
2024-08-13 03:52:02 +0530

    some file changes

--- src/main/java/com/onlineBankingOperations/config/filter/constant/JWTSecurityConstant.java
+++
@@ -1,7 +0,0 @@
-package com.onlineBankingOperations.config.filter.constant;
-
-public final class JWTSecurityConstant {
-
-    public static final String JWT_SECRET_DEFAULT_VALUE = "afafasfafafasfasfasfafacasdasfasxASFACASDFACASDFASFASFDAFASFASDAADSCSDFADCVSGCFVADXCcadwavfsfarvf";
-    public static final String JWT_HEADER = "Authorization";
-}
